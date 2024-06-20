ecd65cf841c0924f155e0ca50bae8e616b331879
ROMA2 <romasecure84@yandex.ru>
2024-06-20 23:14:35 +0400

    Find element by tag name statement added

---
+++ src/test/java/FindTagName.java
@@ -0,0 +1,26 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+public class FindTagName {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.get("https://the-internet.herokuapp.com/tables");
+
+        WebElement table = driver.findElement(By.id("table1"));
+        List<WebElement> tableRows = table.findElements(By.tagName("tr"));
+        System.out.println(tableRows.size());
+        for (int i = 0; i < 5; i++){
+            System.out.println(tableRows.get(i).getText());
+        }
+        Thread.sleep(2000);
+        driver.quit();
+    }
+}
